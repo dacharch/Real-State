70bf37bbae5c5a7cdf53f0fb7154edc1e7eae04c
Neeraj Kumar <nk35715945@gmail.com>
2024-02-26 14:35:21 +0530

    files are added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,47 @@
+
+import {BiMenuAltRight} from 'react-icons/bi' ;
 import "./Header.css"
+import { useState } from 'react';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 export default function Header(){
+    const [menuOpened,setMenuOpened] = useState(false) ;
+
+    function getMenuStyles(menuOpened){
+         if(document.documentElement.clientWidth <=800){
+            return {
+                right :!menuOpened && "-100%" 
+            }
+         }
+
+    }
     return(
         <section className="h-wrapper" style={{ background:"none"}}>
             <div className="flexCenter paddings innerWidth h-container">
                  <img src="./logo.png" alt="" width={100}/>
-                 <div>
-                 <div className="flexCenter h-menu" >
+              
+              < OutsideClickHandler 
+                 onOutsideClick={()=>setMenuOpened(false)}
+              >
+              <div className="flexCenter h-menu"
+                 style={getMenuStyles(menuOpened)}
+                 >
                     <a href="#">Residencies</a>
-                    <a href="#">Residencies</a>
-                    <a href="#">Residencies</a>
-                    <a href="#">Residencies</a>
+                    <a href="#">Our Value</a>
+                    <a href="#">Contact Us</a>
+                    <a href="#">Get Started</a>
                     <button className="button">
                           <a href="">Contact</a>
                     </button>
                  </div>
-                 </div>
+              
+              </OutsideClickHandler>
                 
+                 <div className="menu-icon" onClick={()=>setMenuOpened((prev)=> !prev)}>
+                <BiMenuAltRight size={30} />
             </div>
-
+            
+            </div>
         </section>
     )
 }
